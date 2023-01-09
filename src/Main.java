56ad839a4203c73a3b771d58a5f1f4ab96519df1
Jeanne Lane <jeanne.m.lane8@gmail.com>
2023-01-09 02:00:05 -0500

    changed logic of code back to original

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,59 +9,61 @@ import java.util.HashMap;
 import java.lang.Math;
 import java.awt.Color;
 
+import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
 class Main {
-    static int t_base = 4000;
+    static double t_base = 4000.;
     static List<Map<String, Object>> std_atm = new ArrayList<Map<String, Object>>(Arrays.asList(
             new HashMap<String, Object>() {{
-                put("height", 0);
-                put("pressure", 101325);
-                put("temperature", 15);
+                put("height", 0.);
+                put("pressure", 101325.);
+                put("temperature", 15.);
                 put("sat_vp", 17.1);
             }},
             new HashMap<String, Object>() {{
-                put("height", 1000);
-                put("pressure", 89880);
+                put("height", 1000.);
+                put("pressure", 89880.);
                 put("temperature", 8.5);
                 put("sat_vp", 11.1);
             }},
             new HashMap<String, Object>() {{
-                put("height", 2000);
-                put("pressure", 79500);
-                put("temperature", 2);
+                put("height", 2000.);
+                put("pressure", 79500.);
+                put("temperature", 2.);
                 put("sat_vp", 7.1);
             }},
             new HashMap<String, Object>() {{
-                put("height", 3000);
-                put("pressure", 70012);
+                put("height", 3000.);
+                put("pressure", 70012.);
                 put("temperature", -4.49);
-                put("sat_vp", 0);
+                put("sat_vp", 0.);
             }},
             new HashMap<String, Object>() {{
-                put("height", 4000);
-                put("pressure", 61660);
+                put("height", 4000.);
+                put("pressure", 61660.);
                 put("temperature", -10.98);
-                put("sat_vp", 0);
+                put("sat_vp", 0.);
             }}
     ));
     static List<Map<String, Object>> atm_w_density = new ArrayList<Map<String, Object>>();
     static List<Double> atm_height_list = new ArrayList<Double>();
     static List<Map<String, Object>> graph_list = new ArrayList<Map<String, Object>>();
 
-    static void run() {
+    static void run() throws IOException {
         atm_w_density = build_atm_w_density(std_atm);
         atm_height_list = build_atm_height_list(std_atm);
         int start_i = get_start_idx(t_base, atm_height_list);
         for (int andx = 0; andx < 22; andx++) {
-            int s_angle = 4 * andx;
-            int angle = s_angle;
+            double s_angle = 4 * andx;
+            double angle = s_angle;
             double curr_y = t_base;
             double curr_x = 0;
             int indx = start_i;
@@ -94,7 +98,7 @@ class Main {
             double next_x = get_next_x(curr_x, curr_y, angle, next_y);
             x_list.add(next_x);
             y_list.add(next_y);
-            int next_angle = get_next_angle(angle, indx, atm_w_density, false, next_i);
+            double next_angle = get_next_angle(angle, indx, atm_w_density, false, next_i);
             curr_x = next_x;
             curr_y = next_y;
             indx = next_i;
@@ -109,7 +113,8 @@ class Main {
     print_graph_list(graph_list);
     XYSeriesCollection dataset = new XYSeriesCollection();
 for (Map<String, Object> graph_item : graph_list) {
-        XYSeries series = new XYSeries((int) graph_item.get("s_angle"));
+        double  s_angle = (double) graph_item.get("s_angle");
+        XYSeries series = new XYSeries(s_angle);
         List<Double> x_list = (List<Double>) graph_item.get("x_list");
         List<Double> y_list = (List<Double>) graph_item.get("y_list");
         for (int i = 0; i < x_list.size(); i++) {
@@ -119,7 +124,13 @@ for (Map<String, Object> graph_item : graph_list) {
     }
     JFreeChart chart = ChartFactory.createXYLineChart("Propagation of Thunder in Standard Atmosphere", "Horizontal Distance (m)", "Height (m)", dataset, PlotOrientation.VERTICAL, true, true, false);
 chart.getXYPlot().setBackgroundPaint(Color.WHITE);
+        ValueAxis xAxis = chart.getXYPlot().getDomainAxis();
+        xAxis.setRange(0, 25000); // sets the x-axis range from 0 to 100
     ChartFrame frame = new ChartFrame("Thunder Propagation", chart);
+        int width = 640;
+        int height = 480;
+        File file = new File("chart.png");
+        ChartUtils.saveChartAsPNG(file, chart, width, height);
 frame.pack();
 frame.setVisible(true);
 }
@@ -136,27 +147,28 @@ frame.setVisible(true);
         return curr_x + delta_x;
     }
 
-    static int get_next_angle(int angle, int indx, List<Map<String, Object>> atm_w_density, boolean is_water, int next_i) {
-        Map<String, Object> atm_parameters = atm_w_density.get(indx);
-        double humid_density = (double) atm_parameters.get("humid_density");
-        double dry_density = (double) atm_parameters.get("dry_density");
-        double sat_vp = (double) atm_parameters.get("sat_vp");
-        double sp_sound = (double) atm_parameters.get("sp_sound");
-        double temperature = (double) atm_parameters.get("temperature");
-        double humidity_ratio = (double) ((humid_density / 28.9644) / (dry_density / 28.9644 - humid_density / 28.9644));
-        double pressure = (double) atm_parameters.get("pressure");
-        double humidity = (double) (humidity_ratio / (1 + humidity_ratio));
-        double pressure_in_atm = pressure / 101325;
-        double es = (double) (6.11 * Math.pow(10, (7.5 * temperature) / (237.7 + temperature)));
-        double e = (double) (humidity * pressure / (0.378 * humidity + 0.622));
-        double c = (double) (20.05 * Math.sqrt(e / pressure_in_atm));
-        double a = (double) (c * c / sp_sound);
-        int new_angle = angle;
-        if (is_water) {
-            new_angle = (int) ((angle - 90) - Math.atan(2 * (next_i - indx) * a / c) * 180 / Math.PI + 90);
-        } else {
-            new_angle = (int) (angle - Math.atan(2 * (next_i - indx) * a / c) * 180 / Math.PI);
-        }
+    static double get_next_angle(double angle, int indx, List<Map<String, Object>> atm_w_density, boolean is_water, int next_i) {
+        Map<String, Object> atm_parameters1 = atm_w_density.get(indx);
+        Map<String, Object> atm_parameters2 = atm_w_density.get(next_i);
+        String key;
+        key = (is_water) ? "humid_sp_sound" : "dry_sp_sound";
+
+        double v1 = (double) atm_parameters1.get(key);
+        double v2 = (double) atm_parameters2.get(key);
+        int dens_ndx;
+        double new_angle;
+        //double humidity_ratio = (double) ((humid_density / 28.9644) / (dry_density / 28.9644 - humid_density / 28.9644));
+        //double pressure = (double) atm_parameters.get("pressure");
+        //double humidity = (double) (humidity_ratio / (1 + humidity_ratio));
+        //double pressure_in_atm = pressure / 101325;
+        //double es = (double) (6.11 * Math.pow(10, (7.5 * temperature) / (237.7 + temperature)));
+        //double e = (double) (humidity * pressure / (0.378 * humidity + 0.622)); //specific humidity
+        https://earthscience.stackexchange.com/questions/2360/how-do-i-convert-specific-humidity-to-relative-humidity
+        //double c = (double) (20.05 * Math.sqrt(e / pressure_in_atm));
+        //speed of sound equal to 20.05 times sqrt temp in K
+        // https://www.engineeringtoolbox.com/air-speed-sound-d_603.html
+        //double a = (double) (c * c / sp_sound);
+        new_angle  = getRefraction(angle, v1, v2);
         if (new_angle < 0) {
             new_angle += 360;
         } else if (new_angle > 360) {
@@ -164,30 +176,52 @@ frame.setVisible(true);
         }
         return new_angle;
     }
-
+    public static double getRefraction(double dangle1, double v1, double v2) {
+        double rangle1 = Math.toRadians(dangle1);
+        double expr = Math.sin(rangle1) * v2 / v1;
+        if (expr == 1) {
+            return 90;
+        } else if (expr > 1) {
+            expr = 2 - expr;
+            double rangle2 = Math.asin(expr);
+            double dangle2 = Math.toDegrees(rangle2);
+            return 180 - dangle2;
+        } else {
+            double rangle2 = Math.asin(expr);
+            return Math.toDegrees(rangle2);
+        }
+    }
     static List<Map<String, Object>> build_atm_w_density(List<Map<String, Object>> std_atm) {
         List<Map<String, Object>> atm_w_density = new ArrayList<Map<String, Object>>();
         for (int i = 0; i < std_atm.size(); i++) {
             Map<String, Object> atm_parameters = new HashMap<String, Object>();
             Map<String, Object> std_atm_parameters = std_atm.get(i);
-            double height = (double) std_atm_parameters.get
-                    ("height");
+            double height = (double) std_atm_parameters.get("height");
             double pressure = (double) std_atm_parameters.get("pressure");
             double temperature = (double) std_atm_parameters.get("temperature");
             double sat_vp = (double) std_atm_parameters.get("sat_vp");
             double dry_density = (double) (pressure / (287.058 * (temperature + 273.15)));
             double humid_density = (double) (pressure / (287.058 * (temperature + 273.15) - sat_vp));
-            double sp_sound = (double) (331.3 * Math.sqrt(1 + temperature / 273.15));
+
+            double dry_sp_sound = getSpeedSound(pressure, dry_density);
+            double humid_sp_sound = getSpeedSound(pressure, humid_density);
             atm_parameters.put("height", height);
             atm_parameters.put("pressure", pressure);
             atm_parameters.put("temperature", temperature);
             atm_parameters.put("sat_vp", sat_vp);
             atm_parameters.put("dry_density", dry_density);
             atm_parameters.put("humid_density", humid_density);
-            atm_parameters.put("sp_sound", sp_sound);
+            atm_parameters.put("dry_sp_sound", dry_sp_sound);
+            atm_parameters.put("humid_sp_sound", humid_sp_sound);
             atm_w_density.add(atm_parameters);
         }
         return atm_w_density;
+    }
+
+    private static double getSpeedSound(double pressure, double density) {
+        double kappa = 1.402;
+        double sound = Math.sqrt(kappa * pressure / density);
+        return sound;
     }
 
     static List<Double> build_atm_height_list(List<Map<String, Object>> std_atm) {
@@ -210,7 +244,7 @@ frame.setVisible(true);
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         run();
     }
 }
